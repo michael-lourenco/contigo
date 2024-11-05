c58cf9048e5f9ca548ecb6c98c3b4346f9f08c91
Michael Lourenco <michael.lourenco@pixter.com.br>
2024-11-05 19:58:18 -0300

    Enhance UserInfo component aesthetics and button styles for improved usability
    - Added styling to the container for a more harmonious design
    - Improved text visibility with better typography and colors
    - Adjusted button styles for consistency and accessibility
    - Ensured proper alignment and spacing of elements

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { UserData } from '@/services/firebase/FirebaseService';
+import { Icon } from "./icons";
 
 interface UserInfoProps {
   user: UserData | null;
@@ -10,20 +11,23 @@ interface UserInfoProps {
 
 export const UserInfo: React.FC<UserInfoProps> = ({ user, handleLogin, handleLogout }) => {
   return (
-    <div className="flex justify-between text-slate-50 mb-4">
+    <div className="flex justify-between items-center text-slate-50 mb-4 p-4 bg-slate-800 rounded-lg shadow-lg">
       {user ? (
         <>
-          <span>{user.displayName} - record - {user.best_score}</span>
-          <Button onClick={handleLogout} variant="destructive">
+          <span className="flex items-center text-lg font-semibold">
+            {user.displayName}
+            <Icon name="PiTarget" className="w-6 h-6 text-green-500 mx-2" />
+            <span className="text-green-300">{user.best_score}</span>
+          </span>
+          <Button onClick={handleLogout} variant="destructive" className="ml-4">
             Logout
           </Button>
         </>
       ) : (
-        <Button onClick={handleLogin} variant="default">
-          Sign with Google
+        <Button onClick={handleLogin} variant="primary" className="w-full">
+          Sign in with Google
         </Button>
       )}
     </div>
   );
 };
-
