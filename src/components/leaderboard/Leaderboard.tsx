c86469b071e0a949982cde03a63c7124627dd379
Michael Lourenco <michael.lourenco@pixter.com.br>
2024-11-30 01:12:59 -0300

    update leaderboars list

--- src/components/leaderboard/Leaderboard.tsx
+++ src/components/leaderboard/Leaderboard.tsx
@@ -1,31 +1,27 @@
-import { useState, useEffect } from 'react';
-import { fetchLeaderboards, createLeaderboard, Leaderboard } from '@/services/gamification/api';
-
-export const LeaderboardsList = () => {
+ // components/LeaderboardsList.tsx
+ import { useState, useEffect } from 'react';
+ import { fetchLeaderboards,Leaderboard } from '@/services/gamification/api';
+ 
+ export const LeaderboardsList = () => {
   const [leaderboards, setLeaderboards] = useState<Leaderboard[]>([]);
   const [error, setError] = useState<string | null>(null);
-
+ 
   useEffect(() => {
-    const initializeLeaderboards = async () => {
+    const getLeaderboards = async () => {
       try {
-        console.log("Calling createLeaderboard...");
-        await createLeaderboard(); 
-        console.log("createLeaderboard completed.");
-
-        console.log("Fetching leaderboards...");
+        console.log("getLeaderboards")
         const data = await fetchLeaderboards();
         setLeaderboards(data);
-        console.log("Leaderboards fetched.");
       } catch (err) {
-        setError(err instanceof Error ? err.message : 'Failed to fetch or create leaderboards');
+        setError(err instanceof Error ? err.message : 'Failed to fetch leaderboards');
       }
     };
-
-    initializeLeaderboards();
-  }, []); 
-
+    
+    getLeaderboards();
+  }, []);
+ 
   if (error) return <div>Error: {error}</div>;
-
+ 
   return (
     <div>
       {leaderboards.map((leaderboard) => (
@@ -37,4 +33,4 @@ export const LeaderboardsList = () => {
       ))}
     </div>
   );
-};
+ };
