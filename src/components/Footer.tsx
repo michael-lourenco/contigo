a9d68f12e83848ac67b7a07e5a17cc113aa1a54b
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-01-21 16:46:06 -0300

    add hover and isActive to footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,37 @@
+"use client";
+
 import { Icon } from "./icons";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const menuItems = [
   { icon: "LuHome", label: "Home", href: "/" },
-  { icon: "LuLayoutDashboard", label: "Dashboard", href: "/player" },
   { icon: "LuHelpCircle", label: "Como Jogar", href: "/how_to_play" },
   { icon: "LuUser", label: "Perfil", href: "/player" },
-] as const; // Use "as const" para garantir os tipos corretos.
+] as const;
 
 export function Footer() {
+  const pathname = usePathname(); // Obtém a rota atual
+
   return (
     <footer className="sticky bottom-0 w-full bg-slate-800 p-4 shadow-lg">
       <nav className="flex justify-between items-center max-w-md mx-auto">
-        {menuItems.map((item) => (
-          <Link key={item.href} href={item.href} passHref>
-            <Button
-              variant="ghost"
-              size="sm"
-              className="flex flex-col items-center text-slate-300 hover:text-slate-50"
+        {menuItems.map((item) => {
+          const isActive = pathname === item.href;
+
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={`flex-1 flex flex-col items-center justify-center p-2 text-slate-300 hover:text-slate-50 ${
+                isActive ? "bg-slate-700 text-white" : ""
+              }`}
             >
               <Icon name={item.icon} className="h-6 w-6 mb-1" />
               <span className="text-xs font-medium">{item.label}</span>
-            </Button>
-          </Link>
-        ))}
+            </Link>
+          );
+        })}
       </nav>
     </footer>
   );
